0b84df614eff25f9a961bfb95bc923ab449e58ed
vyskoutaris <vyskoutaris@hotmail.com>
2019-02-01 17:48:32 +0000

    Done.

--- grandma.rb
+++ grandma.rb
@@ -5,8 +5,6 @@ def speak_to_grandma(phrase)
     return "I LOVE YOU TOO PUMPKIN!"
   elsif phrase == phrase.upcase
     return "NO, NOT SINCE 1938!"
-  elsif phrase == "HI!"
-  return "NO, NOT SINCE 1938!"
   elsif phrase == "Hi Nana"
   return "HUH?! SPEAK UP, SONNY"
   else
